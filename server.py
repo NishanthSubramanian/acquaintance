80ed44c287a5fcc9396fccda41f091167f3027cd
Rohit M P <rohitmp99@gmail.com>
2019-08-08 00:54:07 +0530

    Added backend code for login; incomplete; needs testing

--- server.py
+++ server.py
@@ -1,5 +1,15 @@
+#!/usr/bin/env python
+
 from flask import Flask, render_template, redirect, url_for, request
+from flask_mysqldb import MySQL
+import hashlib
+
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'acq'
+mysql = MySQL(app)
 
 
 @app.route('/news_feed')
@@ -13,28 +23,37 @@ def chat():
 
 
 @app.route('/')
-def identify():
-    return render_template('identify.html')
-
-
-@app.route('/credentials', methods=['GET', 'POST'])
-def credentials():
-    if request.method == 'POST':
-        # IF present go to login for password
-        if request.form['email'] == 'admin@admin.com':
-            return render_template('login.html', email=request.form['email'])
-        else:   # otherwise go to signup
-            return render_template('signup.html', email=request.form['email'])
-    return
+def login():
+    return render_template('login.html')
 
 
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+
+@app.route('/verifylogin', methods=['GET', 'POST'])
+def verifylogin():
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        email = request.form['email']
+        password = request.form['pass']
+        passwordhash = hashlib.md5(password)
+        cur.execute('select * from login_credentials')
+        db = cur.fetchall() #fetch all entries which satisfied the query
+        cur.close()
+        emailexists = 0
+        for entry in db:
+            for dbemail, dbpassword in entry:
+                if email == dbemail: #if email is already registered
+                    emailexists = 1
+                    if password == dbpassword:
+                        return render_template('news_feed.html') #pass email to identify the user here
+        if emailexists == 0: #lead to signup page if email is not registered
+            return render_template('signup.html', email=request.form['email'])
+        else: #return something that says either email or password is wrong
+            return 
+    return
 
 
 if __name__ == '__main__':

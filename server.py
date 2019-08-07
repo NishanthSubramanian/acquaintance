c06fbdda31fbd3855cc66daf356dad6126cd3ea0
Rohit M P <rohitmp99@gmail.com>
2019-08-08 01:35:53 +0530

    added backend code for signup and login; routing has issues

--- server.py
+++ server.py
@@ -31,24 +31,40 @@ def login():
 def signup():
     return render_template('signup.html')
 
+@app.route('/register_user', methods=['GET', 'POST'])
+def register_user():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['pass']
+        passwordhash = hashlib.md5(password.encode()).hexdigest()
+        username = request.form['username']
+        #no school
+        cur = mysql.connection.cursor()
+        cur.execute('insert into login_credentials values(%s, %s)', [email, passwordhash])
+        mysql.connection.commit()
+        #update in profile table too
+        cur.close()
+    return render_template('news_feed.html')
 
-@app.route('/verifylogin', methods=['GET', 'POST'])
-def verifylogin():
+
+@app.route('/verify_login', methods=['GET', 'POST'])
+def verify_login():
     if request.method == 'POST':
         cur = mysql.connection.cursor()
         email = request.form['email']
         password = request.form['pass']
-        passwordhash = hashlib.md5(password)
+        passwordhash = hashlib.md5(password.encode()).hexdigest()
         cur.execute('select * from login_credentials')
         db = cur.fetchall() #fetch all entries which satisfied the query
         cur.close()
         emailexists = 0
-        for entry in db:
-            for dbemail, dbpassword in entry:
-                if email == dbemail: #if email is already registered
-                    emailexists = 1
-                    if password == dbpassword:
-                        return render_template('news_feed.html') #pass email to identify the user here
+        for dbentry in db:
+            dbemail = dbentry[0]
+            dbpassword = dbentry[1]
+            if email == dbemail: #if email is already registered
+                emailexists = 1
+                if password == dbpassword:
+                    return render_template('news_feed.html') #pass email to identify the user here
         if emailexists == 0: #lead to signup page if email is not registered
             return render_template('signup.html', email=request.form['email'])
         else: #return something that says either email or password is wrong
